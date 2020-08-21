dda0920172f42439b5f909197ca109de33afd4e3
Maksym Liannoi <liannoi444@gmail.com>
2020-08-21 17:50:25 +0300

    Prepared boilerplate

---
+++ app/src/main/java/org/itstep/liannoi/sampleandroid/presentation/tasks/TasksActivity.kt
@@ -0,0 +1,13 @@
+package org.itstep.liannoi.sampleandroid.presentation.tasks
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import org.itstep.liannoi.sampleandroid.R
+
+class TasksActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tasks)
+    }
+}
